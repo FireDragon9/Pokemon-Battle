6068e5b25eaec769734848413fa30df426d6a07f
Dragon9 <silvacchio@gmail.com>
2024-09-02 21:47:33 -0300

    Fixed errors with function declarations in iniPokemon.cpp; More variables and functions in Pokemon.h;

--- Pokemon.cpp
+++ Pokemon.cpp
@@ -1,9 +1,47 @@
 #include "Pokemon.h"
-
+#include "SFML/include/Graphics.hpp"
 #include <string>
 
-Pokemon::Pokemon(std::string name) { setName(name); };
+Pokemon::Pokemon(std::string name, int lvl, int hp, int atk, int def, int spAtk,
+                 int spDef, int spd) {
+  // setting the information
+  setName(name);
+  setLevel(lvl);
 
+  // setting the stats
+  Stats.setHP(hp);
+};
+
+// POKEMON INFORMATION
 void Pokemon::setName(std::string &name) { pkmName = name; }
 
-std::string Pokemon::getName() const { return pkmName; }
+void Pokemon::setLevel(int &lvl) { level = lvl; }
+
+// POKEMON STATS
+
+// SETTERS
+void Pokemon::Stats::setHP(int &hp) { HP = hp; }
+
+void Pokemon::Stats::setAttack(int &atk) { Attack = atk; }
+
+void Pokemon::Stats::setDef(int &def) { Defense = def; }
+
+void Pokemon::Stats::setSpAttack(int &spAtk) { spAttack = spAtk; }
+
+void Pokemon::Stats::setSpDef(int &spDef) { spDefense = spDef; }
+
+void Pokemon::Stats::setSpd(int &spd) { Speed = spd; }
+
+// GETTERS
+
+int Pokemon::Stats::getHP() { return HP; }
+
+int Pokemon::Stats::getAttack() { return Attack; }
+
+int Pokemon::Stats::getDef() { return Defense; }
+
+int Pokemon::Stats::getSpAttack() { return spAttack; }
+
+int Pokemon::Stats::getSpDef() { return spDefense; }
+
+int Pokemon::Stats::getSpd() { return Speed; }
--- Pokemon.h
+++ Pokemon.h
@@ -1,6 +1,11 @@
 #ifndef POKEMON_H
 #define POKEMON_H
 
+#include "SFML/include/Graphics.hpp"
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Text.hpp>
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/System/String.hpp>
 #include <string>
 
 class Pokemon {
@@ -8,6 +13,7 @@ private:
   std::string pkmName;
   int level;
 
+  ////////////////////////
   class Stats {
 
   private:
@@ -16,33 +22,74 @@ private:
     int Defense;
     int spAttack;
     int spDefense;
-    int speed;
+    int Speed;
 
   public:
     // SETTERS
 
-    void setHP(int hp);
-    void setAttack(int attack);
-    void setDef(int def);
-    void setSpAttack(int spAttack);
-    void setSpd(int Spd);
+    void setHP(int &hp);
+    void setAttack(int &atk);
+    void setDef(int &def);
+    void setSpAttack(int &spAtk);
+    void setSpDef(int &spDef);
+    void setSpd(int &spd);
 
+    // GETTERS
     int getHP();
     int getAttack();
     int getDef();
     int getSpAttack();
+    int getSpDef();
     int getSpd();
 
   } Stats; // Anonymous class for abilities
 
+  /////////////////////////
+  class Draw {
+
+  private:
+    sf::Texture pkmPartyTexture;
+    sf::Texture pkmFrontTexture;
+    sf::Texture pkmBackTexture;
+
+    sf::Sprite pkmPartySprite;
+    sf::Sprite pkmFrontSprite;
+    sf::Sprite pkmBackSprite;
+
+  public:
+    // getters
+    sf::Texture getPartyTxt();
+    sf::Texture getFrontTxt();
+    sf::Texture getBackTexture();
+
+    sf::Sprite getPartySpr();
+    sf::Sprite getFrontSpr();
+    sf::Sprite getBackSpr();
+
+    // setters
+    void setPartyTxt(std::string partyTxt);
+    void setFrontTxt(std::string frontTxt);
+    void setBackTxt(std::string backTxt);
+
+    void setPartySpr(sf::Texture partyTxt);
+    void setFrontSpr(sf::Texture frontTxt);
+    void setBackSpr(sf::Texture backTxt);
+
+  } Draw;
+
+  /////////////////////////
+
 public:
   // constructor
-  Pokemon(std::string name);
+  Pokemon(std::string name, int lvl, int hp, int atk, int def, int spAtk,
+          int spDef, int spd);
 
+  // setters
   void setName(std::string &name);
 
-  void setLevel();
+  void setLevel(int &lvl);
 
+  // getters
   std::string getName() const;
 };
 
